ac9b7929e350e2bfc6782cb8b1634d3359b431e3
klangner <klangner@gmail.com>
2021-12-09 19:56:28 +0100

    HumanvsHuman

--- src/main/scala/connectx/Console.scala
+++ src/main/scala/connectx/Console.scala
@@ -17,3 +17,4 @@ object Console:
             } + "|"
             println(rowLine)
         println(line)
+        println(" " + 1.to(board.width).mkString(" "))
--- src/main/scala/connectx/app/HumanVsHuman.scala
+++ src/main/scala/connectx/app/HumanVsHuman.scala
@@ -1,15 +1,36 @@
 package connectx.app
 
+import scala.io.StdIn.readLine
+
 import connectx.game.{Board}
 import connectx.Console
 import connectx.game.StoneColor
+import scala.annotation.tailrec
 
 object HumanVsHuman:
   def main(args: Array[String]): Unit = 
     val board = Board(7, 6)
-    board.putStone(3, StoneColor.Black)
-    board.putStone(3, StoneColor.White)
-    board.putStone(3, StoneColor.Black)
-    board.putStone(2, StoneColor.White)
+
+    play(board, StoneColor.Black)
+
+  @tailrec
+  def play(board: Board, stone: StoneColor): Unit = 
     Console.printBoard(board)
+    if (stone == StoneColor.Black)
+      println("black move: ")
+    else
+      println("white move: ")
+    val col = safeInt(readLine()).getOrElse(0) - 1
+    if (col < 0) return
+    board.putStone(col, stone)
+    play(board, if(stone == StoneColor.Black) StoneColor.White else StoneColor.Black)
+
+  
+  def safeInt(str: String): Option[Int] =
+    try
+      Some(str.toInt)
+    catch 
+      case e: Exception => None
+  
+
 
