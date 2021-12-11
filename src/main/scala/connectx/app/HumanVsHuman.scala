5acc3b431b38c5e0643e880b43f21958b99cc8c0
klangner <klangner@gmail.com>
2021-12-11 22:17:32 +0100

    human vs human can win game

--- src/main/scala/connectx/app/HumanVsHuman.scala
+++ src/main/scala/connectx/app/HumanVsHuman.scala
@@ -4,6 +4,7 @@ import scala.io.StdIn.readLine
 
 import connectx.game.{Board, StoneColor, GameResult}
 import scala.annotation.tailrec
+import connectx.game.VictoryChecker
 
 object HumanVsHuman:
   def main(args: Array[String]): Unit = 
@@ -21,13 +22,13 @@ object HumanVsHuman:
   @tailrec
   def play(board: Board, stone: StoneColor, lastPass: Boolean): GameResult = 
     println(board.mkString())
-    if (stone == StoneColor.Black)
-      print("black move: ")
-    else
-      print("white move: ")
+    print(s"$stone move: ")
     val col = safeInt(readLine()).getOrElse(0) - 1
     val isPass = !board.putStone(col, stone)
     if(lastPass && isPass) return GameResult.Draw
+    if(VictoryChecker.hasWon(board, stone))
+      println(board.mkString())
+      return if(stone == StoneColor.Black) GameResult.BlackWon else GameResult.WhiteWon
     val nextColor = if(stone == StoneColor.Black) StoneColor.White else StoneColor.Black
     play(board, nextColor, isPass)
 
