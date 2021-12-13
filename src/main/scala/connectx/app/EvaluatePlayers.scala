71f9045a1725b86163cb081afb3d151958e5b622
klangner <klangner@gmail.com>
2021-12-13 17:19:22 +0100

    Evaluate agent via playing with other agent

--- src/main/scala/connectx/app/EvaluatePlayers.scala
+++ src/main/scala/connectx/app/EvaluatePlayers.scala
@@ -7,36 +7,44 @@ import connectx.game.VictoryChecker
 import connectx.agent.{Agent, Action, HumanPlayer, RandomAgent}
 
 
-object PlayGame:
+object EvaluatePlayers:
 
   def main(args: Array[String]): Unit = 
-    runGame(args(0), args(1))
+    val numGames = 10_000
+    val player1 = args(0)
+    val player2 = args(1)
+    val startTime = System.currentTimeMillis
+    // Player1 == black
+    val results1 = 0.until(numGames/2).map(_ => runGame(player1, player2))
+    // Player2 == black
+    val results2 = 0.until(numGames/2).map(_ => runGame(player2, player1))
+    val player1Wins = 
+      results1.filter(_ == GameResult.BlackWon).length + results2.filter(_ == GameResult.WhiteWon).length
+    val player2Wins = 
+      results1.filter(_ == GameResult.WhiteWon).length + results2.filter(_ == GameResult.BlackWon).length
+    val draws = numGames - (player1Wins + player2Wins)
+    val totalTime = (System.currentTimeMillis - startTime) / 1000
+    println(s"Played $numGames in $totalTime")
+    println(s"Player1 ($player1) wins: $player1Wins (${100*player1Wins/numGames}%)")
+    println(s"Player2 ($player2) wins: $player2Wins (${100*player2Wins/numGames}%)")
+    println(s"Black wins: $draws (${100*draws/numGames}%)")
 
 
-  def runGame(bot1: String, bot2: String): Unit = 
+  def runGame(bot1: String, bot2: String): GameResult = 
     val board = Board(7, 6)
     val blackPlayer = initAgent(bot1, StoneColor.Black)
     val whitePlayer = initAgent(bot2, StoneColor.White)
-    val result = play(board, blackPlayer, whitePlayer, StoneColor.Black, false)
-    println("")
-    result match 
-      case GameResult.BlackWon => println("Black won!")
-      case GameResult.WhiteWon => println("White won!")
-      case _ => println("Draw")
-
-    println("")
+    play(board, blackPlayer, whitePlayer, StoneColor.Black, false)
 
 
   def initAgent(name: String, color: StoneColor): Agent = 
     name match
       case "random" => RandomAgent(color)
-      case _        => HumanPlayer(color)
+      case _        => RandomAgent(color)
 
 
   @tailrec
   def play(board: Board, blackPlayer: Agent, whitePlayer: Agent, stone: StoneColor, lastPass: Boolean): GameResult = 
-    println(board.mkString())
-    println(s"$stone move: ")
     val player = if (stone == StoneColor.Black) blackPlayer else whitePlayer 
     val action = makeMove(board, player) 
     val (newBoard, isPass) = action match
@@ -44,7 +52,6 @@ object PlayGame:
       case Action.PutStone(col) => (board.putStone(col, stone), false)
     if(lastPass && isPass) return GameResult.Draw
     if(VictoryChecker.hasWon(newBoard, stone))
-      println(newBoard.mkString())
       return if(stone == StoneColor.Black) GameResult.BlackWon else GameResult.WhiteWon
     val nextColor = if(stone == StoneColor.Black) StoneColor.White else StoneColor.Black
     play(newBoard, blackPlayer, whitePlayer, nextColor, isPass)
